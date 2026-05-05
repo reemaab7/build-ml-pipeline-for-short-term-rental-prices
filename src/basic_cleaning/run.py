ca75ae379f34e9a1a4c8158762877dd4bb9459fa
reemaab7 <reema.subaie123@gmail.com>
2026-05-05 15:54:43 +0300

    Complete ML pipeline with all steps

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,58 @@
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    logger.info("Downloading artifact")
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+
+    df = pd.read_csv(artifact_path)
+
+    logger.info("Removing price outliers")
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+
+    logger.info("Converting last_review to datetime")
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    logger.info("Removing rows outside NYC boundaries")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
+    logger.info("Saving cleaned data")
+    df.to_csv("clean_sample.csv", index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+    logger.info("Artifact uploaded successfully")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+    parser.add_argument("--input_artifact", type=str, help="Name of the input artifact", required=True)
+    parser.add_argument("--output_artifact", type=str, help="Name of the output artifact", required=True)
+    parser.add_argument("--output_type", type=str, help="Type of the output artifact", required=True)
+    parser.add_argument("--output_description", type=str, help="Description of the output artifact", required=True)
+    parser.add_argument("--min_price", type=float, help="Minimum price to consider", required=True)
+    parser.add_argument("--max_price", type=float, help="Maximum price to consider", required=True)
+
+    args = parser.parse_args()
+    go(args)
